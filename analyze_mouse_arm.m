692d8c154fc94f7f6bc6a945a49aced318d5659b
Katie Andre <90425541+kmandr22@users.noreply.github.com>
2022-10-25 15:46:37 -0400

    Update analyze_mouse_arm.m

--- analyze_mouse_arm.m
+++ analyze_mouse_arm.m
@@ -3,20 +3,32 @@ function [a, b, c, d] = analyze_mouse_arm(directory)
 % analyze (knockout or wildtype) and examines all the experiments and
 % returns the parameters a, b, c, and d. 
 
-mycwd = pwd
-mydir = 'mouse_data'
-myexp = directory
-file = 'mouse_arm_data.txt'
-n = 001
+mycwd = pwd;
+mydir = 'mouse_data';
+myexp = directory;
+file = 'mouse_arm_data.txt';
+n = 001;
 
 
 for n = 001:length(directory)
-trial = ['exp' n]
+if n<10
+    trial = append('exp00', num2str(n));
+end
+if n<20
+    trial = append('exp01', num2str(n));
+end
+if n<30
+    trial = append('exp02', num2str(n));
+end
+if n<40
+    trial = append('exp03', num2str(n));
+end
 myfilename = fullfile(mycwd,mydir,myexp, trial, file);
 
-[a, b, c, d] = fit_arm_nerve(myfilename)
+[a, b, c, d] = fit_arm_nerve(myfilename);
+
 
 clear myfilename
-n = n+1
+n = n+1;
 end
 end
